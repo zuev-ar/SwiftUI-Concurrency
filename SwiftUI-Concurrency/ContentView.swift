94c32133993f4f3b3e692670a9bbe203cbffb5ff
Arkadii Zuev <arkasha.zuev@icloud.com>
2022-02-08 19:13:57 +0500

    Initial Commit

---
+++ SwiftUI-Concurrency/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Concurrency
+//
+//  Created by Arkasha Zuev on 08.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Concurrency/SwiftUI_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ConcurrencyApp.swift
+//  SwiftUI-Concurrency
+//
+//  Created by Arkasha Zuev on 08.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
